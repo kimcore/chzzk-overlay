dfe8ee2e931bf7b2f2d58f928c3cf46234d3b3b9
kimcore <kimcoreovo@gmail.com>
2023-12-20 22:39:58 +0900

    feat: use appendChild, add 5ms chat delay

--- app/[channelId]/chat.tsx
+++ app/[channelId]/chat.tsx
@@ -1,11 +1,9 @@
-// noinspection JSIgnoredPromiseFromCall
-
 "use client"
 
-import {Fragment, useEffect, useState} from "react"
+import {useEffect} from "react"
 import {useSearchParams} from "next/navigation"
 import {clsx} from "clsx"
-import {ChatEvent, ChzzkChat} from "chzzk"
+import {ChatCmd} from "chzzk"
 
 const colors = [
     "rgb(219, 74, 63)",
@@ -18,64 +16,98 @@ const colors = [
 
 const emojiRegex = /{:([a-zA-Z0-9_]+):}/g
 
-function Chat({badges, color, nickname, emojis, message}) {
-    const match = message.match(emojiRegex)
+async function sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms))
 
-    return (
-        <div data-from={nickname}>
-            <span className="meta" style={{
-                color: typeof color == "number" ? colors[color] : color
-            }}>
-                {badges.map((badge: { name: string, src: string }) => (
-                    <img alt={badge.name} key={badge.name} className="badge" src={badge.src}/>
-                ))}
-                <span className="name">
-                    {nickname}
-                </span>
-                <span className="colon">
-                    :
-                </span>
-            </span>
-            <span className="message">
-                {match ? (
-                    <Fragment>
-                        {message.split(emojiRegex).map((part: string, i: number) => {
-                            if (i % 2 == 0) {
-                                return part
-                            } else {
-                                const src = emojis[part]
-                                return (
-                                    <span key={i} className="emote_wrap">
-                                        <img alt={`{:${part}:}`} className="emoticon" src={src}/>
-                                    </span>
-                                )
-                            }
-                        })}
-                    </Fragment>
-                ) : message}
-            </span>
-        </div>
-    )
 }
 
 export default function ChatBox({chatChannelId, accessToken}) {
-    const [chats, setChats] = useState([])
-
     const searchParams = useSearchParams()
     const small = searchParams.has("small")
 
-    function onChat(chat: ChatEvent) {
-        if (chat.hidden) return
+    const defaults = {
+        cid: chatChannelId,
+        svcid: "game",
+        ver: "2"
+    }
 
-        const id = `${chat.profile.userIdHash}-${chat.time}`
+    function appendChat({nickname, badges, color, emojis, message}) {
+        const elem = document.createElement("div")
+        elem.setAttribute("data-from", nickname)
+
+        const meta = document.createElement("span")
+        meta.className = "meta"
+        meta.style.color = typeof color == "number" ? colors[color] : color
+
+        badges.forEach(badge => {
+            const img = document.createElement("img")
+            img.className = "badge"
+            img.alt = badge.name
+            img.src = badge.src
+            meta.appendChild(img)
+        })
+
+        const name = document.createElement("span")
+        name.className = "name"
+        name.innerText = nickname
+        meta.appendChild(name)
+
+        const colon = document.createElement("span")
+        colon.className = "colon"
+        colon.innerText = ":"
+        meta.appendChild(colon)
+
+        elem.appendChild(meta)
+
+        const msg = document.createElement("span")
+        msg.className = "message"
+
+        const match = message.match(emojiRegex)
+
+        if (match) {
+            message.split(emojiRegex).forEach((part, i) => {
+                if (i % 2 == 0) {
+                    msg.appendChild(document.createTextNode(part))
+                } else {
+                    const src = emojis[part]
+                    const emoteWrap = document.createElement("span")
+                    emoteWrap.className = "emote_wrap"
+                    const emote = document.createElement("img")
+                    emote.className = "emoticon"
+                    emote.alt = `{:${part}:}`
+                    emote.src = src
+                    emoteWrap.appendChild(emote)
+                    msg.appendChild(emoteWrap)
+                }
+            })
+        } else {
+            msg.innerText = message
+        }
+
+        elem.appendChild(msg)
+
+        const log = document.getElementById("log")
+
+        log.appendChild(elem)
+
+        if (log.children.length > 50) {
+            log.removeChild(log.children[0])
+        }
+
+        window.scrollTo(0, document.body.scrollHeight)
+    }
+
+    function onChat(chat) {
+        if ((chat['msgStatusType'] || chat['messageStatusType']) == "HIDDEN") return
+
         const nickname = chat.profile.nickname
         const badge = chat.profile.badge ? {
             name: chat.profile.title.name, src: chat.profile.badge.imageUrl
         } : null
         const badges = (badge ? [badge] : []).concat(
             chat.profile.activityBadges
-                .filter(badge => badge.activated)
-                .map(badge => ({name: badge.title, src: badge.imageUrl}))
+                ?.filter(badge => badge.activated)
+                ?.map(badge => ({name: badge.title, src: badge.imageUrl})) ?? []
         )
         const color = chat.profile.title?.color ?? nickname.split("")
             .map(c => c.charCodeAt(0))
@@ -83,43 +115,91 @@ export default function ChatBox({chatChannelId, accessToken}) {
         const emojis = chat.extras?.emojis || {}
         const message = chat.message
 
-        setChats((prevState) => {
-            const newChats = prevState.concat([{
-                id,
-                badges,
-                color,
-                nickname,
-                emojis,
-                message
-            }])
-
-            if (newChats.length > 50) {
-                newChats.splice(0, newChats.length - 50)
-            }
-
-            return newChats
+        appendChat({
+            nickname,
+            badges,
+            color,
+            emojis,
+            message
         })
     }
 
-    useEffect(() => {
-        const chzzkChat = ChzzkChat.fromAccessToken(chatChannelId, accessToken)
-        chzzkChat.on("chat", onChat.bind(this))
-        chzzkChat.on("connect", () => chzzkChat.requestRecentChat(50))
-        chzzkChat.on("disconnect", () => {
-            setChats([])
-            chzzkChat.connect()
-        })
+    async function onMessage(event: MessageEvent) {
+        const json = JSON.parse(event.data)
 
-        chzzkChat.connect()
+        switch (json.cmd) {
+            case ChatCmd.PING:
+                this.send(JSON.stringify({
+                    cmd: ChatCmd.PONG,
+                    tid: json.tid
+                }))
+                break
+            case ChatCmd.CONNECTED:
+                const sid = json.bdy.sid
+                this.send(JSON.stringify({
+                    bdy: {recentMessageCount: 50},
+                    cmd: ChatCmd.REQUEST_RECENT_CHAT,
+                    sid,
+                    tid: 2,
+                    ...defaults
+                }))
+                break
+            case ChatCmd.RECENT_CHAT:
+            case ChatCmd.CHAT:
+                const isRecent = json.cmd == ChatCmd.RECENT_CHAT
+                const chats = (isRecent ? json['bdy']['messageList'] : json['bdy'])
+                    .filter(chat => (chat['msgTypeCode'] || chat['messageTypeCode']) == 1)
+
+                for (const chat of chats) {
+                    const profile = JSON.parse(chat['profile'])
+                    const extras = JSON.parse(chat['extras'])
+
+                    if (!isRecent) {
+                        await sleep(5)
+                    }
+
+                    onChat({
+                        profile,
+                        extras,
+                        message: chat['msg'] || chat['content'],
+                    })
+                }
+
+                break
+        }
+    }
+
+    function connectChzzk() {
+        const ws = new WebSocket("wss://kr-ss1.chat.naver.com/chat")
+
+        ws.onopen = () => {
+            ws.send(JSON.stringify({
+                bdy: {
+                    accTkn: accessToken,
+                    auth: "READ",
+                    devType: 2001,
+                    uid: null
+                },
+                cmd: ChatCmd.CONNECT,
+                tid: 1,
+                ...defaults
+            }))
+        }
+
+        ws.onclose = () => {
+            setTimeout(() => {
+                connectChzzk()
+            }, 1000)
+        }
+
+        ws.onmessage = onMessage
+    }
+
+    useEffect(() => {
+        connectChzzk()
     }, [])
 
-    useEffect(() => {
-        window.scrollTo(0, document.body.scrollHeight)
-    }, [chats])
-
     return (
-        <div id="log" className={clsx(small && "small")}>
-            {chats.map(chat => <Chat key={chat.id} {...chat}/>)}
-        </div>
+        <div id="log" className={clsx(small && "small")}/>
     )
 }
