ff8425c5889c3610ebe4b7260270a6de52827aee
kimcore <kimcoreovo@gmail.com>
2023-12-20 20:07:28 +0900

    fix: access token request being cached

--- app/[channelId]/page.tsx
+++ app/[channelId]/page.tsx
@@ -1,13 +1,17 @@
 import Chat from "@/app/[channelId]/chat"
 
 export default async function ChatPage({params: {channelId}}) {
-    const chatChannelId = await fetch(`https://api.chzzk.naver.com/polling/v1/channels/${channelId}/live-status`)
-        .then(r => r.json())
-        .then(data => data['content']['chatChannelId'])
+    const {signal} = new AbortController()
 
-    const accessToken = await fetch(`https://comm-api.game.naver.com/nng_main/v1/chats/access-token?channelId=${chatChannelId}&chatType=STREAMING`)
-        .then(r => r.json())
-        .then(data => data['content']['accessToken'])
+    const chatChannelId = await fetch(
+        `https://api.chzzk.naver.com/polling/v1/channels/${channelId}/live-status`,
+        {signal}
+    ).then(r => r.json()).then(data => data['content']['chatChannelId'])
+
+    const accessToken = await fetch(
+        `https://comm-api.game.naver.com/nng_main/v1/chats/access-token?channelId=${chatChannelId}&chatType=STREAMING`,
+        {signal}
+    ).then(r => r.json()).then(data => data['content']['accessToken'])
 
     return (
         <Chat chatChannelId={chatChannelId} accessToken={accessToken}/>
