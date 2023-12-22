c6d0c4a85f7bd46f7f018bea7aa0057c8ba7a8c6
kimcore <kimcoreovo@gmail.com>
2023-12-22 16:24:26 +0900

    feat: match chzzk chat color

--- app/[channelId]/chat.tsx
+++ app/[channelId]/chat.tsx
@@ -5,14 +5,7 @@ import {useSearchParams} from "next/navigation"
 import {clsx} from "clsx"
 import {ChatCmd} from "chzzk"
 
-const colors = [
-    "rgb(219, 74, 63)",
-    "rgb(95, 158, 160)",
-    "rgb(218, 165, 32)",
-    "rgb(0, 255, 127)",
-    "rgb(180, 84, 255)",
-    "rgb(30, 144, 255)"
-]
+const colors = ["#ECA843", "#EEA05D", "#EA723D", "#EAA35F", "#E98158", "#E97F58", "#E76D53", "#E66D5F", "#E56B79", "#E16490", "#E481AE", "#E68199", "#DC5E9A", "#E16CB5", "#D25FAC", "#D263AE", "#D66CB4", "#D071B6", "#BA82BE", "#AF71B5", "#A96BB2", "#905FAA", "#B38BC2", "#9D78B8", "#8D7AB8", "#7F68AE", "#9F99C8", "#717DC6", "#5E7DCC", "#5A90C0", "#628DCC", "#7994D0", "#81A1CA", "#ADD2DE", "#80BDD3", "#83C5D6", "#8BC8CB", "#91CBC6", "#83C3BB", "#7DBFB2", "#AAD6C2", "#84C194", "#B3DBB4", "#92C896", "#94C994", "#9FCE8E", "#A6D293", "#ABD373", "#BFDE73", "#CCE57D"]
 
 const emojiRegex = /{:([a-zA-Z0-9_]+):}/g
 
@@ -109,7 +102,7 @@ export default function ChatBox({chatChannelId, accessToken}) {
                 ?.filter(badge => badge.activated)
                 ?.map(badge => ({name: badge.title, src: badge.imageUrl})) ?? []
         )
-        const color = chat.profile.title?.color ?? nickname.split("")
+        const color = chat.profile.title?.color ?? (chat.profile.userIdHash + chatChannelId).split("")
             .map(c => c.charCodeAt(0))
             .reduce((a, b) => a + b, 0) % colors.length
         const emojis = chat.extras?.emojis || {}
