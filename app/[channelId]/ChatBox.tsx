a1ca8cdcac993b923610e63c91754881a18bee41
Ahn Kiwook <ahnkiwook@gmail.com>
2023-12-23 04:50:45 +0900

    Use buffer instead of sleep

--- app/[channelId]/ChatBox.tsx
+++ app/[channelId]/ChatBox.tsx
@@ -6,53 +6,13 @@ import {clsx} from "clsx"
 import {ChatCmd} from "chzzk"
 import ChatRow, {Chat, nicknameColors} from "./ChatRow"
 
-async function sleep(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms))
-
-}
-
 export default function ChatBox({chatChannelId, accessToken}) {
     const searchParams = useSearchParams()
     const small = searchParams.has("small")
 
     const isClosingWebSocket = useRef<boolean>(false)
+    const pendingChatListRef = useRef<Chat[]>([])
     const [chatList, setChatList] = useState<Chat[]>([])
-
-    const appendChat = useCallback((chat: Chat) => {
-        setChatList((prevChatList) => {
-            const newChatList = [...prevChatList, chat]
-            if (newChatList.length > 50) {
-                newChatList.shift()
-            }
-            return newChatList
-        })
-    }, [])
-
-    function onChat(chat) {
-        const nickname = chat.profile.nickname
-        const badge = chat.profile.badge ? {
-            name: chat.profile.title.name, src: chat.profile.badge.imageUrl
-        } : null
-        const badges = (badge ? [badge] : []).concat(
-            chat.profile.activityBadges
-                ?.filter(badge => badge.activated)
-                ?.map(badge => ({name: badge.title, src: badge.imageUrl})) ?? []
-        )
-        const color = chat.profile.title?.color ?? (chat.profile.userIdHash + chatChannelId).split("")
-            .map(c => c.charCodeAt(0))
-            .reduce((a, b) => a + b, 0) % nicknameColors.length
-        const emojis = chat.extras?.emojis || {}
-        const message = chat.message
-
-        appendChat({
-            uuid: crypto.randomUUID(),
-            nickname,
-            badges,
-            color,
-            emojis,
-            message
-        })
-    }
 
     function connectChzzk() {
         const ws = new WebSocket("wss://kr-ss1.chat.naver.com/chat")
@@ -85,7 +45,7 @@ export default function ChatBox({chatChannelId, accessToken}) {
             }
         }
 
-        ws.onmessage = async (event: MessageEvent) => {
+        ws.onmessage = (event: MessageEvent) => {
             const json = JSON.parse(event.data)
 
             switch (json.cmd) {
@@ -112,23 +72,34 @@ export default function ChatBox({chatChannelId, accessToken}) {
                         .filter(chat => (chat['msgTypeCode'] || chat['messageTypeCode']) == 1)
                         .filter(chat => !((chat['msgStatusType'] || chat['messageStatusType']) == "HIDDEN"))
                         .sort((a, b) => a.msgTime - b.msgTime)
-
-                    for (let i = 0; i < chats.length; i++) {
-                        const chat = chats[i]
-                        const profile = JSON.parse(chat['profile'])
-                        const extras = JSON.parse(chat['extras'])
-
-                        if (!isRecent) {
-                            await sleep(i * 75)
-                        }
-
-                        onChat({
-                            profile,
-                            extras,
-                            message: chat['msg'] || chat['content'],
+                        .map(chat => {
+                            const profile = JSON.parse(chat['profile'])
+                            const extras = JSON.parse(chat['extras'])
+                            const nickname = profile.nickname
+                            const badge = profile.badge ? {
+                                name: profile.title.name, src: profile.badge.imageUrl
+                            } : null
+                            const badges = (badge ? [badge] : []).concat(
+                                profile.activityBadges
+                                    ?.filter(badge => badge.activated)
+                                    ?.map(badge => ({name: badge.title, src: badge.imageUrl})) ?? []
+                            )
+                            const color = profile.title?.color ?? (profile.userIdHash + chatChannelId).split("")
+                                .map(c => c.charCodeAt(0))
+                                .reduce((a, b) => a + b, 0) % nicknameColors.length
+                            const emojis = extras?.emojis || {}
+                            const message = chat['msg'] || chat['content']
+                            return {
+                                uuid: crypto.randomUUID(),
+                                nickname,
+                                badges,
+                                color,
+                                emojis,
+                                message
+                            }
                         })
-                    }
 
+                    pendingChatListRef.current = [...pendingChatListRef.current, ...chats].slice(-50)
                     break
             }
         }
@@ -147,6 +118,22 @@ export default function ChatBox({chatChannelId, accessToken}) {
             window.location.reload()
         })
         return connectChzzk()
+    }, [])
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (pendingChatListRef.current.length > 0) {
+                const chat = pendingChatListRef.current.shift()
+                setChatList((prevChatList) => {
+                    const newChatList = [...prevChatList, chat]
+                    if (newChatList.length > 50) {
+                        newChatList.shift()
+                    }
+                    return newChatList
+                })
+            }
+        }, 75)
+        return () => clearInterval(interval)
     }, [])
 
     return (
